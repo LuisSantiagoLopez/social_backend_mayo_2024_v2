9936a4c0018d1a7fbcec2134f716c01d570a040a
LuisSantiagoLopez <luis.santiago.l.sanchez@gmail.com>
2024-05-22 20:45:02 -0600

    development server not running on the server, changed debug to true temporarily

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = env.bool('DEBUG', default=False)
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = ["socialapi.xyz", "localhost", "127.0.0.1", "104.131.185.142"]
 
