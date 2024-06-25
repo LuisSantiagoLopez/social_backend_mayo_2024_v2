f6504feaed20510831819d02623dc9b9f5a8d8e4
LuisSantiagoLopez <luis.santiago.l.sanchez@gmail.com>
2024-06-25 15:24:35 -0600

    changed serializer to show lat and lon

--- antros/serializers.py
+++ antros/serializers.py
@@ -3,26 +3,40 @@ from .models import Antro, MenuItem, Review
 from django.contrib.gis.geos import Point
 
 
+from rest_framework import serializers
+from .models import Antro
+from django.contrib.gis.geos import Point
+
 class AntroSerializer(serializers.ModelSerializer):
-    latitude = serializers.FloatField(write_only=True)
-    longitude = serializers.FloatField(write_only=True)
+    latitude = serializers.SerializerMethodField()
+    longitude = serializers.SerializerMethodField()
+    latitude_input = serializers.FloatField(write_only=True, required=False)
+    longitude_input = serializers.FloatField(write_only=True, required=False)
 
     class Meta:
         model = Antro
-        fields = ['id', 'user', 'image', 'name', 'description', 'contact', 'approved', 'category', 'cost', 'latitude', 'longitude']
+        fields = ['id', 'user', 'image', 'name', 'description', 'contact', 'approved', 'category', 'cost', 'latitude', 'longitude', 'latitude_input', 'longitude_input']
+
+    def get_latitude(self, obj):
+        return obj.location.y if obj.location else None
+
+    def get_longitude(self, obj):
+        return obj.location.x if obj.location else None
 
     def create(self, validated_data):
-        latitude = validated_data.pop('latitude')
-        longitude = validated_data.pop('longitude')
-        validated_data['location'] = Point(longitude, latitude, srid=4326)
+        latitude = validated_data.pop('latitude_input', None)
+        longitude = validated_data.pop('longitude_input', None)
+        if latitude is not None and longitude is not None:
+            validated_data['location'] = Point(longitude, latitude, srid=4326)
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
-        latitude = validated_data.pop('latitude', None)
-        longitude = validated_data.pop('longitude', None)
+        latitude = validated_data.pop('latitude_input', None)
+        longitude = validated_data.pop('longitude_input', None)
         if latitude is not None and longitude is not None:
             instance.location = Point(longitude, latitude, srid=4326)
         return super().update(instance, validated_data)
+
 
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
