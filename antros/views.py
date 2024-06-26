369df0e1c38cd8f3968a15348836a5004bd974ac
LuisSantiagoLopez <luis.santiago.l.sanchez@gmail.com>
2024-06-26 14:27:49 -0600

    solo usuarios pueden crear antros a su nombre

--- antros/views.py
+++ antros/views.py
@@ -11,13 +11,11 @@ from django.contrib.gis.geos import Point
 
 class AntroViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAntroOwnerOrReadOnly,)
+    queryset = Antro.objects.all()
     serializer_class = AntroSerializer
 
     def get_queryset(self):
         return Antro.objects.filter(user=self.request.user)
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
 
 class MenuItemViewSet(viewsets.ModelViewSet):
     permission_classes = (IsMenuItemAntroOwnerOrReadOnly,)
